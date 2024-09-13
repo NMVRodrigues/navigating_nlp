6aff17c4c8ee7eccfa10f82b6d0bb5503920d8a2
Nuno <nuno.mvrodrigues1@gmail.com>
2024-09-13 18:06:33 +0100

    attention implemented

--- scripts/attention_from_scratch.py
+++ scripts/attention_from_scratch.py
@@ -26,13 +26,36 @@ vector_representations = embedding_layer(input_ids)
 
 print(f'Original token vectors shape: {vector_representations.shape}')
 
+
+#
+#
+# REGULAR WAY TO DO IT
+#
+#
+
 post_attention_vectors = []
 
 for v in tqdm(vector_representations, desc='Computing attention...'):
+    # compte the doct product between v and all other vectors
     dot_products = torch.tensor([torch.dot(v, vx) for vx in vector_representations])
-    scores = torch.softmax(dot_products, dim=0)
-    post_attention_vectors.append(torch.add(torch.matmul(scores, vector_representations)))
+    # compute the attention weights for all v.vx pairs
+    attention_scores = torch.softmax(dot_products, dim=0)
+    # multiply the weights by each of the original vectors and sum them up to get the new vector
+    post_attention_vectors.append((attention_scores.view(-1, 1) * vector_representations).sum(dim=0))
 
-post_attention_vectors = torch.tensor(post_attention_vectors)
+post_attention_vectors = torch.stack(post_attention_vectors)
 
 print(f'Post attention token vectors shape: {post_attention_vectors.shape}')
+
+#
+#
+# UTILIZING K Q V
+#
+#
+
+K, Q, V = vector_representations, vector_representations, vector_representations
+
+attention_scores = torch.softmax(K @ Q.T, dim=1)
+post_attention_vectors_kqv = attention_scores @ V
+
+print(f'Post attention token vectors utilizing KQV shape: {post_attention_vectors_kqv.shape}')
